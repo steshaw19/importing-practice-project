0f0193507f80d7cdaff5b1c8b3a8e2bebc5a017e
Ste <steshaw19@gmail.com>
2023-10-22 21:21:03 +0100

    First commit.

---
+++ alias.py
@@ -0,0 +1,4 @@
+from time import time as time_time
+from datetime import time as datetime_time
+print(datetime_time())
+print(time_time())
---
+++ main.py
@@ -0,0 +1,7 @@
+## In main.py, import x from module_2 and print it. 
+## Test that it works by running python main.py in the terminal. 
+##You should see the message 'Hello, I am in module 2'.
+
+from module_2 import x
+x = "Hello, I am in main.py"
+print(x)
